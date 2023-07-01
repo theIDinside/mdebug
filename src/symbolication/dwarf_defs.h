d9c0d9b34383ad9ff89a3272192f3dc7d2f87959
Simon Farre <simon.farre.cx@gmail.com>
2023-07-01 19:56:28 +0200

    Added attribute form & tags

--- src/symbolication/dwarf_defs.h
+++ src/symbolication/dwarf_defs.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstdint>
 
-enum class Attribute : std::uint32_t
+enum class Attribute : std::uint16_t
 {
   DW_AT_sibling = 0x01,
   DW_AT_location = 0x02,
@@ -147,52 +147,52 @@ enum class Attribute : std::uint32_t
   DW_AT_hi_user = 0x3fff,
 };
 
-enum class AttributeForm : std::uint32_t
+enum class AttributeForm : std::uint8_t
 {
-  DW_FORM_addr = 0x01,
-  Reserved = 0x02,
-  DW_FORM_block2 = 0x03,
-  DW_FORM_block4 = 0x04,
-  DW_FORM_data2 = 0x05,
-  DW_FORM_data4 = 0x06,
-  DW_FORM_data8 = 0x07,
-  DW_FORM_string = 0x08,
-  DW_FORM_block = 0x09,
-  DW_FORM_block1 = 0x0a,
-  DW_FORM_data1 = 0x0b,
-  DW_FORM_flag = 0x0c,
-  DW_FORM_sdata = 0x0d,
-  DW_FORM_strp = 0x0e,
-  DW_FORM_udata = 0x0f,
-  DW_FORM_ref_addr = 0x10,
-  DW_FORM_ref1 = 0x11,
-  DW_FORM_ref2 = 0x12,
-  DW_FORM_ref4 = 0x13,
-  DW_FORM_ref8 = 0x14,
-  DW_FORM_ref_udata = 0x15,
-  DW_FORM_indirect = 0x16,
-  DW_FORM_sec_offset = 0x17,
-  DW_FORM_exprloc = 0x18,
-  DW_FORM_flag_present = 0x19,
-  DW_FORM_strx = 0x1a,
-  DW_FORM_addrx = 0x1b,
-  DW_FORM_ref_sup4 = 0x1c,
-  DW_FORM_strp_sup = 0x1d,
-  DW_FORM_data16 = 0x1e,
-  DW_FORM_line_strp = 0x1f,
-  DW_FORM_ref_sig8 = 0x20,
-  DW_FORM_implicit_const = 0x21,
-  DW_FORM_loclistx = 0x22,
-  DW_FORM_rnglistx = 0x23,
-  DW_FORM_ref_sup8 = 0x24,
-  DW_FORM_strx1 = 0x25,
-  DW_FORM_strx2 = 0x26,
-  DW_FORM_strx3 = 0x27,
-  DW_FORM_strx4 = 0x28,
-  DW_FORM_addrx1 = 0x29,
-  DW_FORM_addrx2 = 0x2a,
-  DW_FORM_addrx3 = 0x2b,
-  DW_FORM_addrx4 = 0x2c,
+  DW_FORM_addr = 0x01,            // 0b0000'0001
+  Reserved = 0x02,                // 0b0000'0010
+  DW_FORM_block2 = 0x03,          // 0b0000'0011
+  DW_FORM_block4 = 0x04,          // 0b0000'0100
+  DW_FORM_data2 = 0x05,           // 0b0000'0101
+  DW_FORM_data4 = 0x06,           // 0b0000'0110
+  DW_FORM_data8 = 0x07,           // 0b0000'0111
+  DW_FORM_string = 0x08,          // 0b0000'1000
+  DW_FORM_block = 0x09,           // 0b0000'1001
+  DW_FORM_block1 = 0x0a,          // 0b0000'1010
+  DW_FORM_data1 = 0x0b,           // 0b0000'1011
+  DW_FORM_flag = 0x0c,            // 0b0000'1100
+  DW_FORM_sdata = 0x0d,           // 0b0000'1101
+  DW_FORM_strp = 0x0e,            // 0b0000'1110
+  DW_FORM_udata = 0x0f,           // 0b0000'1111
+  DW_FORM_ref_addr = 0x10,        // 0b0001'0001        ~( 0b0001'0111 ) -> 0b1110'1000
+  DW_FORM_ref1 = 0x11,            // 0b0001'0010
+  DW_FORM_ref2 = 0x12,            // 0b0001'0011
+  DW_FORM_ref4 = 0x13,            // 0b0001'0100
+  DW_FORM_ref8 = 0x14,            // 0b0001'0101
+  DW_FORM_ref_udata = 0x15,       // 0b0001'0110
+  DW_FORM_indirect = 0x16,        // 0b0001'0111
+  DW_FORM_sec_offset = 0x17,      // 0b0001'1000
+  DW_FORM_exprloc = 0x18,         // 0b0001'1001
+  DW_FORM_flag_present = 0x19,    // 0b0001'1010
+  DW_FORM_strx = 0x1a,            // 0b0001'1011
+  DW_FORM_addrx = 0x1b,           // 0b0001'1100
+  DW_FORM_ref_sup4 = 0x1c,        // 0b0001'1101
+  DW_FORM_strp_sup = 0x1d,        // 0b0001'1110
+  DW_FORM_data16 = 0x1e,          // 0b0001'1111
+  DW_FORM_line_strp = 0x1f,       // 0b0000'0001
+  DW_FORM_ref_sig8 = 0x20,        // 0b0000'0001
+  DW_FORM_implicit_const = 0x21,  // 0b0000'0001
+  DW_FORM_loclistx = 0x22,        // 0b0000'0001
+  DW_FORM_rnglistx = 0x23,        // 0b0000'0001
+  DW_FORM_ref_sup8 = 0x24,        // 0b0000'0001
+  DW_FORM_strx1 = 0x25,           // 0b0000'0001
+  DW_FORM_strx2 = 0x26,           // 0b0000'0001
+  DW_FORM_strx3 = 0x27,           // 0b0000'0001
+  DW_FORM_strx4 = 0x28,           // 0b0000'0001
+  DW_FORM_addrx1 = 0x29,          // 0b0000'0001
+  DW_FORM_addrx2 = 0x2a,          // 0b0000'0001
+  DW_FORM_addrx3 = 0x2b,          // 0b0000'0001
+  DW_FORM_addrx4 = 0x2c,          // 0b0000'0001
 };
 
 enum class BaseTypeAttributeEncoding : std::uint32_t
@@ -369,4 +369,84 @@ enum class DwarfUnitType : std::uint8_t
   DW_UT_split_type = 0x06,
   DW_UT_lo_user = 0x80,
   DW_UT_hi_user = 0xff,
+};
+
+enum class DwarfTag : std::uint16_t {
+    DW_TAG_array_type = 0x01,
+    DW_TAG_class_type = 0x02,
+    DW_TAG_entry_point = 0x03,
+    DW_TAG_enumeration_type = 0x04,
+    DW_TAG_formal_parameter = 0x05,
+    // Reserved0x6 = 0x06,
+    // Reserved0x7 = 0x07,
+    DW_TAG_imported_declaration = 0x08,
+    // Reserved = 0x09,
+    DW_TAG_label = 0x0a,
+    DW_TAG_lexical_block = 0x0b,
+    // Reserved = 0x0c,
+    DW_TAG_member = 0x0d,
+    // Reserved = 0x0e,
+    DW_TAG_pointer_type = 0x0f,
+    DW_TAG_reference_type = 0x10,
+    DW_TAG_compile_unit = 0x11,
+    DW_TAG_string_type = 0x12,
+    DW_TAG_structure_type = 0x13,
+    // Reserved = 0x14,
+    DW_TAG_subroutine_type = 0x15,
+    DW_TAG_typedef = 0x16,
+    DW_TAG_union_type = 0x17,
+    DW_TAG_unspecified_parameters = 0x18,
+    DW_TAG_variant = 0x19,
+    DW_TAG_common_block = 0x1a,
+    DW_TAG_common_inclusion = 0x1b,
+    DW_TAG_inheritance = 0x1c,
+    DW_TAG_inlined_subroutine = 0x1d,
+    DW_TAG_module = 0x1e,
+    DW_TAG_ptr_to_member_type = 0x1f,
+    DW_TAG_set_type = 0x20,
+    DW_TAG_subrange_type = 0x21,
+    DW_TAG_with_stmt = 0x22,
+    DW_TAG_access_declaration = 0x23,
+    DW_TAG_base_type = 0x24,
+    DW_TAG_catch_block = 0x25,
+    DW_TAG_const_type = 0x26,
+    DW_TAG_constant = 0x27,
+    DW_TAG_enumerator = 0x28,
+    DW_TAG_file_type = 0x29,
+    DW_TAG_friend = 0x2a,
+    DW_TAG_namelist = 0x2b,
+    DW_TAG_namelist_item = 0x2c,
+    DW_TAG_packed_type = 0x2d,
+    DW_TAG_subprogram = 0x2e,
+    DW_TAG_template_type_parameter = 0x2f,
+    DW_TAG_template_value_parameter = 0x30,
+    DW_TAG_thrown_type = 0x31,
+    DW_TAG_try_block = 0x32,
+    DW_TAG_variant_part = 0x33,
+    DW_TAG_variable = 0x34,
+    DW_TAG_volatile_type = 0x35,
+    DW_TAG_dwarf_procedure = 0x36,
+    DW_TAG_restrict_type = 0x37,
+    DW_TAG_interface_type = 0x38,
+    DW_TAG_namespace = 0x39,
+    DW_TAG_imported_module = 0x3a,
+    DW_TAG_unspecified_type = 0x3b,
+    DW_TAG_partial_unit = 0x3c,
+    DW_TAG_imported_unit = 0x3d,
+    // Reserved = 0x3e1,
+    DW_TAG_condition = 0x3f,
+    DW_TAG_shared_type = 0x40,
+    DW_TAG_type_unit = 0x41,
+    DW_TAG_rvalue_reference_type = 0x42,
+    DW_TAG_template_alias = 0x43,
+    DW_TAG_coarray_type = 0x44,        // ‡
+    DW_TAG_generic_subrange = 0x45,    // ‡
+    DW_TAG_dynamic_type = 0x46,        // ‡
+    DW_TAG_atomic_type = 0x47,         // ‡
+    DW_TAG_call_site = 0x48,           // ‡
+    DW_TAG_call_site_parameter = 0x49, // ‡
+    DW_TAG_skeleton_unit = 0x4a,       // ‡
+    DW_TAG_immutable_type = 0x4b,      // ‡
+    DW_TAG_lo_user = 0x4080,
+    DW_TAG_hi_user = 0xffff,
 };
