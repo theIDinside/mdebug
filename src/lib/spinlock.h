32e19dbf1d360a4f8e264c19a3b95700d54d280a
Simon Farre <simon.farre.cx@gmail.com>
2023-07-05 19:19:07 +0200

    Added src folder lib
    Put "library" stuff here. Things that aren't related to debugging or debug info /debug symbols.
    
    It's my hope, that this will facilitate moving to using better software in the future, by pulling in external dependencies for instance.

---
+++ src/lib/spinlock.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <atomic>
+
+class SpinLock
+{
+public:
+  SpinLock();
+  void lock() noexcept;
+  void unlock() noexcept;
+
+private:
+  std::atomic<unsigned int> m_flag;
+};
