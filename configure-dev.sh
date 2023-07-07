d8d15b35140b7ce4c2340f0866c20503f38c5c2e
Simon Farre <simon.farre.cx@gmail.com>
2023-07-07 13:47:58 +0200

    Added JSON dependency
    Added dependency fetching to MDB dev setup tooling.

--- configure-dev.sh
+++ configure-dev.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 fmt_version="10.0.0"
-
+json_version="v3.11.2"
 # The relative path to repo root dir from where this script is executed,
 # makes it safe to run from where ever.
 relative_repo_root_dir="$(dirname "$0")"
 
+# Begin by setting up libfmt dependencies
 if [ ! -e "$relative_repo_root_dir/dependencies/fmt" ]; then
   wget -P $relative_repo_root_dir/dependencies https://github.com/fmtlib/fmt/releases/download/$fmt_version/fmt-$fmt_version.zip
   unzip $relative_repo_root_dir/dependencies/fmt-$fmt_version.zip -d $relative_repo_root_dir/dependencies/
@@ -14,6 +15,17 @@ if [ ! -e "$relative_repo_root_dir/dependencies/fmt" ]; then
   echo "libfmt dependency met"
 else
   echo "libfmt dependency already met"
+fi
+
+# Setup nlohmann_json dependencies
+if [ ! -e "$relative_repo_root_dir/dependencies/nlohmann_json" ]; then
+  wget -P $relative_repo_root_dir/dependencies "https://github.com/nlohmann/json/releases/download/$json_version/json.tar.xz"
+  tar xvf $relative_repo_root_dir/dependencies/json.tar.xz
+  mv $relative_repo_root_dir/dependencies/json $relative_repo_root_dir/dependencies/nlohmann_json
+  rm $relative_repo_root_dir/dependencies/json.tar.xz
+  echo "json dependency met"
+else
+  echo "json already dependency met"
 fi
 
 # Setup pre-commit formatting requirement hooks
