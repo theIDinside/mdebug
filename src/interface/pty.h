55a1eeb4fb16757f63106c608affa88f740d8028
Simon Farre <simon.farre.cx@gmail.com>
2023-07-09 00:10:27 +0200

    Added pseudo-terminal creation
    We can now start actually look like a DAP-debugger.

---
+++ src/interface/pty.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <expected>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <variant>
+
+struct MasterPty
+{
+  std::string name;
+  int fd;
+};
+
+struct PtyParentResult
+{
+  std::string pty_name;
+  pid_t pid;
+  int fd;
+};
+
+std::optional<MasterPty> open_pty_master() noexcept;
+std::variant<pid_t, PtyParentResult> pty_fork(const termios *slave_termios, const winsize *slave_winsize);
