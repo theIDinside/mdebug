c827e5fad66395fb88ddf69c29a27c0e90bc6cf9
Simon Farre <simon.farre.cx@gmail.com>
2023-07-10 16:07:35 +0200

    Added DAP definitions

--- src/interface/dap/dap_defs.h
+++ src/interface/dap/dap_defs.h
@@ -56,4 +56,82 @@ to_str(Events command) noexcept
 #undef DAP_EVENTS
 }
 
+enum class ProtocolMessage : std::uint8_t
+{
+#define DAP_PROTOCOL_MESSAGE
+#define ITEM(name, value) name = value,
+#include "dap.defs"
+#undef ITEM
+#undef DAP_PROTOCOL_MESSAGE
+};
+
+constexpr std::string_view
+to_str(ProtocolMessage msg) noexcept
+{
+#define DAP_PROTOCOL_MESSAGE
+#define ITEM(name, value)                                                                                         \
+  case ProtocolMessage::name:                                                                                     \
+    return #name;
+  switch (msg) {
+#include "dap.defs"
+  }
+#undef ITEM
+#undef DAP_PROTOCOL_MESSAGE
+}
+
+enum class StoppedReason : std::uint8_t
+{
+#define DAP_STOPPED_REASON
+#define ITEM(name, value) name = value,
+#include "dap.defs"
+#undef ITEM
+#undef DAP_STOPPED_REASON
+};
+
+constexpr std::string_view
+to_str(StoppedReason reason) noexcept
+{
+  switch (reason) {
+  case StoppedReason::Step:
+    return "step";
+  case StoppedReason::Breakpoint:
+    return "breakpoint";
+  case StoppedReason::Exception:
+    return "exception";
+  case StoppedReason::Pause:
+    return "pause";
+  case StoppedReason::Entry:
+    return "entry";
+  case StoppedReason::Goto:
+    return "goto";
+  case StoppedReason::FunctionBreakpoint:
+    return "function breakpoint";
+  case StoppedReason::DataBreakpoint:
+    return "data breakpoint";
+  case StoppedReason::InstructionBreakpoint:
+    return "instruction breakpoint";
+  }
+}
+
+// unfortunately, the DAP people were so "brilliant" as to not make the names
+// source code friendly. Amazing. Therefore we need to either hand roll them like this
+// or write a generator that takes names like this and turns them into the un-sourcecode friendly versions
+enum class ThreadReason : std::uint8_t
+{
+  Started,
+  Exited
+};
+
+constexpr std::string_view
+to_str(ThreadReason reason) noexcept
+{
+  switch (reason) {
+  case ThreadReason::Started:
+    return "started";
+  case ThreadReason::Exited:
+    return "exited";
+    break;
+  }
+}
+
 } // namespace ui::dap
