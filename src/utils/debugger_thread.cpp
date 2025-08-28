43fabe61e9162e0861e9fba3458b4bcbdcb2c2b3
Simon Farre <simon.farre.cx@gmail.com>
2025-08-29 00:18:17 +0200

    Open signalfd only for native-launched targets
    This also moves init of the signalfd fd until we actually fork+exec a target.
    
    As such we need to also make sure to block SIGCHLD in the DebuggerThread's when they start, since it's not automatically blocked now.

--- src/utils/debugger_thread.cpp
+++ src/utils/debugger_thread.cpp
@@ -22,6 +22,20 @@ DebuggerThread::~DebuggerThread() noexcept
 }
 
 /* static */
+void
+DebuggerThread::BlockSigchildMustSucceed() noexcept
+{
+  // Block SIGCHLD in this thread to handle it only via signalfd
+  sigset_t mask;
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGCHLD);
+  if (sigprocmask(SIG_BLOCK, &mask, nullptr) == -1) {
+    perror("sigprocmask");
+    PANIC("Blocking SIGCHLD must succeed for MDB to work.");
+  }
+}
+
+/* static */
 std::unique_ptr<DebuggerThread>
 DebuggerThread::SpawnDebuggerThread(std::function<void(std::stop_token &)> task) noexcept
 {
@@ -45,9 +59,9 @@ void
 DebuggerThread::Start() noexcept
 {
   MDB_ASSERT(mStarted == false, "Thread already started");
-  AssertSigChildIsBlocked();
   mStarted = true;
   mThread = std::jthread([this](std::stop_token token) {
+    BlockSigchildMustSucceed();
     // Be doubly-certain that we have not meddled with SIGCHLD. This is of utmost importance to make signalfd
     // work.
     AssertSigChildIsBlocked();
