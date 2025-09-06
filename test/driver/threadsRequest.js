886b0f189714ecf795f5a14fef3ccb2ce50718a8
Simon Farre <simon.farre.cx@gmail.com>
2025-09-06 12:29:06 +0200

    Added requests to test suite
    continueRequest, nextRequest
    Added waitForEvent that takes a predicate and resolves when it returns true, instead of the prepareWaitForEventN command which is harder to use nicely.

--- test/driver/threadsRequest.js
+++ test/driver/threadsRequest.js
@@ -8,45 +8,45 @@ async function threads(debugAdapter) {
   await debugAdapter.startRunToMain(debugAdapter.buildDirFile('threads_shared'), seconds(1))
   let threads = await debugAdapter.threads()
   const file = readFileContents(repoDirFile('test/threads_shared.cpp'))
-  const bp_lines = ['BP1']
+  const breakpointLines = ['BP1']
     .map((ident) => getLineOf(file, ident))
     .filter((item) => item != null)
     .map((l) => ({ line: l }))
-  const breakpointsResponse = await debugAdapter.sendReqGetResponse('setBreakpoints', {
+
+  const res = await debugAdapter.setBreakpointsRequest({
     source: {
       name: repoDirFile('test/threads_shared.cpp'),
       path: repoDirFile('test/threads_shared.cpp'),
     },
-    breakpoints: bp_lines,
+    breakpoints: breakpointLines,
   })
 
-  assertLog(breakpointsResponse[0].verified, 'Expected breakpoint to be ok', 'Breakpoint could not be verified')
+  assertLog(res.body.breakpoints[0].verified, `Expected breakpoint to be ok`, 'Breakpoint could not be verified')
 
-  await debugAdapter.contNextStop(threads[0].id)
+  let waitForStop = debugAdapter.waitForEvent('stopped', 3000, (event) => {
+    return (event.reason == 'breakpoint' && event.threadId == threads[0].id) || event.allThreadsStopped
+  })
+  await debugAdapter.continueRequest({ threadId: threads[0].id }).then(() => waitForStop)
+  await waitForStop
+
   threads = await debugAdapter.threads()
-  let frames = await debugAdapter.stackTrace(threads[1].id, 1000)
-  console.log(`next line for ${threads[1].id}: (${JSON.stringify(threads[1], null, 2)})`)
-  const { event_body, response } = await debugAdapter.sendReqWaitEvent(
-    'next',
-    {
-      threadId: threads[1].id,
-      singleThread: true,
-      granularity: 'line',
-    },
-    'stopped',
-    500
-  )
+  let frames = await threads[1].stacktrace(1000)
 
-  assert(response.success, `Expected 'next' command to succeed; got ${JSON.stringify(response)}`)
+  let nextFinisihed = debugAdapter.waitForEvent('stopped', 3000, (event) => {
+    return event.reason === 'step'
+  })
+
+  let response = await debugAdapter.nextRequest({ threadId: threads[1].id, granularity: 'line', singleThread: true })
+  assertLog(response.success, `Expected 'next' command to succeed`, `Response was ${JSON.stringify(response)}`)
+  await nextFinisihed
 
   frames = await debugAdapter.stackTrace(threads[1].id)
-  console.log(`Stack frames for ${threads[1].id}: ${JSON.stringify(frames, null, 2)}`)
-  const end_line = frames.body.stackFrames[0].line
-  assert(
-    end_line == bp_lines[0].line + 1,
-    `Expected to be at line ${bp_lines[0].line + 1} but we're at line ${end_line}: ${prettyJson(
-      frames.body.stackFrames
-    )}`
+
+  const currentLine = frames.body.stackFrames[0].line
+  assertLog(
+    currentLine == breakpointLines[0].line + 1,
+    `Expected to be at line ${breakpointLines[0].line + 1}`,
+    `Was at line ${currentLine}. \n${prettyJson(frames.body.stackFrames)}`
   )
 }
 const tests = {
