cbe1e7c5c856e630f8be016e55092bceb8145f24
Simon Farre <simon.farre.cx@gmail.com>
2025-10-03 15:38:42 +0200

    Make finishRequest use new debugAdapter.assert interface

--- test/driver/finishRequest.js
+++ test/driver/finishRequest.js
@@ -1,27 +1,30 @@
 const { readFileContents, repoDirFile, getLineOf } = require('./client')
-const { prettyJson, assert } = require('./utils')
+const { prettyJson } = require('./utils')
 
-async function finishSuccess(DA) {
-  await DA.startRunToMain(DA.buildDirFile('stackframes'))
+/**
+ * @param { import("./client").DebugAdapterClient } debugAdapter
+ */
+async function finishSuccess(debugAdapter) {
+  await debugAdapter.startRunToMain(debugAdapter.buildDirFile('stackframes'))
   const file = readFileContents(repoDirFile('test/stackframes.cpp'))
   const bp_lines = ['BP3']
     .map((ident) => getLineOf(file, ident))
     .filter((item) => item != null)
     .map((l) => ({ line: l }))
-  const res = await DA.sendReqGetResponse('setBreakpoints', {
+  const res = await debugAdapter.sendReqGetResponse('setBreakpoints', {
     source: {
       name: repoDirFile('test/stackframes.cpp'),
       path: repoDirFile('test/stackframes.cpp'),
     },
     breakpoints: bp_lines,
   })
-  const threads = await DA.threads()
-  await DA.contNextStop(threads[0].id)
-  console.log(`request stack trace`);
+  const threads = await debugAdapter.threads()
+  await debugAdapter.contNextStop(threads[0].id)
+  console.log(`request stack trace`)
 
-  let frames = await DA.stackTrace(threads[0].id)
+  let frames = await debugAdapter.stackTrace(threads[0].id)
   const next_up_frame = frames.body.stackFrames[1]
-  const { event_body, response } = await DA.sendReqWaitEvent(
+  const { event_body, response } = await debugAdapter.sendReqWaitEvent(
     'stepOut',
     {
       threadId: threads[0].id,
@@ -40,41 +43,47 @@ async function finishSuccess(DA) {
     )}`
   )
 
-  assert(response.success, `Request was unsuccessful: ${JSON.stringify(response)}`)
-  assert(
+  await debugAdapter.assert(response.success, `Expected success-response`, `Response: ${JSON.stringify(response)}`)
+
+  await debugAdapter.assert(
     event_body.reason == 'step',
-    `Expected to see a 'stopped' event with 'step' as reason. Got event ${prettyJson(event_body)}`
+    `Expected to see 'stopped' event with 'step' as reason`,
+    `Got event: ${prettyJson(event_body)}`
   )
 
-  frames = await DA.stackTrace(threads[0].id)
+  frames = await debugAdapter.stackTrace(threads[0].id)
   console.log(`Stopped at ${JSON.stringify(frames.body.stackFrames[0], null, 2)}`)
-  assert(
+  await debugAdapter.assert(
     frames.body.stackFrames[0].line == next_up_frame.line,
-    `Expected to be at line ${next_up_frame.line} but was at ${frames.body.stackFrames[0].line}`
+    `Expected to be at line ${next_up_frame.line}`,
+    `But was at ${frames.body.stackFrames[0].line}`
   )
 }
 
-async function abortedDueToBkpt(DA) {
-  await DA.startRunToMain(DA.buildDirFile('stackframes'))
+/**
+ * @param { import("./client").DebugAdapterClient } debugAdapter
+ */
+async function abortedDueToBkpt(debugAdapter) {
+  await debugAdapter.startRunToMain(debugAdapter.buildDirFile('stackframes'))
   const file = readFileContents(repoDirFile('test/stackframes.cpp'))
   const bp_lines = ['BPLine1', 'BP3']
     .map((ident) => getLineOf(file, ident))
     .filter((item) => item != null)
     .map((l) => ({ line: l }))
-  const res = await DA.sendReqGetResponse('setBreakpoints', {
+  const res = await debugAdapter.sendReqGetResponse('setBreakpoints', {
     source: {
       name: repoDirFile('test/stackframes.cpp'),
       path: repoDirFile('test/stackframes.cpp'),
     },
     breakpoints: bp_lines,
   })
-  const threads = await DA.threads()
-  await DA.contNextStop(threads[0].id)
+  const threads = await debugAdapter.threads()
+  await debugAdapter.contNextStop(threads[0].id)
 
-  let frames = await DA.stackTrace(threads[0].id)
+  let frames = await debugAdapter.stackTrace(threads[0].id)
   const next_up_frame = frames.body.stackFrames[1]
 
-  const { event_body, response } = await DA.sendReqWaitEvent(
+  const { event_body, response } = await debugAdapter.sendReqWaitEvent(
     'stepOut',
     {
       threadId: threads[0].id,
@@ -85,19 +94,19 @@ async function abortedDueToBkpt(DA) {
     1000
   )
 
-  assert(response.success, `Request was unsuccessful: ${JSON.stringify(response)}`)
-  assert(
+  await debugAdapter.assert(response.success, `Expected success response`, `Got: ${JSON.stringify(response)}`)
+  await debugAdapter.assert(
     event_body.reason == 'breakpoint',
-    `Expected to see a 'stopped' event with 'step' as reason. Got event ${prettyJson(event_body)}`
+    `Expected to see a 'stopped' event with 'step' as reason.`,
+    `Got event ${prettyJson(event_body)}`
   )
 
-  frames = await DA.stackTrace(threads[0].id)
+  frames = await debugAdapter.stackTrace(threads[0].id)
 
-  assert(
+  await debugAdapter.assert(
     frames.body.stackFrames[0].line == bp_lines[1].line,
-    `Expected to have hit breakpoint before finishing function. Stack frame ${prettyJson(
-      frames.body.stackFrames[0]
-    )} when we should have been at line ${bp_lines[1].line}`
+    `Expected to have hit breakpoint before finishing function!`,
+    `Stack frame ${prettyJson(frames.body.stackFrames[0])} when we should have been at line ${bp_lines[1].line}`
   )
 }
 
